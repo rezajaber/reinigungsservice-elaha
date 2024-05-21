ba1362ee1b7f79a0df6434730846651fcd37b05e
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-21 17:13:25 +0200

    add font-family

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "../components/Header.vue";
+import Hero from "../components/Hero.vue";
 </script>
 
 <template>
-  <div>
+  <div class="">
     <Header />
+    <Hero class="mt-14 md:mt-28" />
   </div>
 </template>
