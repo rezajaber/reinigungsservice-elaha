36a5938a963edf167edb72294760664a444aaeb4
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-22 04:49:08 +0200

    Finish Leistung-Section

---
+++ src/components/Leistungen.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { EllipsisVertical } from "lucide-vue-next";
+</script>
+
+<template>
+  <!-- Container for the entire section with responsive maximum width and spacing -->
+  <div class="mx-auto grid max-w-[1080px] gap-4 lg:gap-8">
+    <!-- Header section with title and description -->
+    <div class="grid gap-3">
+      <h3
+        class="break-words text-center text-[26px] font-bold leading-7 tracking-wide text-[#03B4CB]"
+      >
+        Unsere Leistungen für Gewerbereinigung in Hamburg
+      </h3>
+      <p class="text-center font-light leading-7 tracking-wide">
+        S&N Gebäudereinigung steht für maßgeschneiderte Reinigungslösungen mit
+        Fokus auf Nachhaltigkeit und Effizienz für Hamburger Gewerbe. Verlassen
+        Sie sich auf unsere Expertise für ein hygienisch reines und optisch
+        einladendes Geschäftsumfeld.
+      </p>
+    </div>
+
+    <!-- Grid of services with responsive columns and spacing -->
+    <div class="mb-5 grid auto-rows-auto gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <!-- Loop to create each service item -->
+      <div v-for="i in 9" :key="i">
+        <img
+          src="../assets/img/leistung.jpg"
+          class="rounded-t-lg"
+          alt="Leistungen"
+        />
+        <p
+          class="rounded-b-lg bg-[#02B4CB] py-3.5 text-center text-xl font-semibold text-white"
+        >
+          Gebäudereinigung
+        </p>
+      </div>
+    </div>
+
+    <!-- Additional services section with highlighted information -->
+    <div
+      class="grid gap-4 rounded-lg bg-[#E5F5F7] px-3.5 py-10 text-center md:p-10 md:text-start"
+    >
+      <div class="flex items-center justify-center gap-2 md:justify-start">
+        <EllipsisVertical class="stroke-[#03B4CB]" />
+        <p class="text-xl font-semibold">
+          Weitere Reinigungsleistungen in Hamburg
+        </p>
+      </div>
+      <p class="break-words text-lg font-light leading-7">
+        Unser umfassendes Reinigungsangebot deckt eine breite Palette an
+        spezialisierten Dienstleistungen ab, darunter
+        <b
+          >Schul- und Kindergartenreinigung, Fitnessstudio- und
+          Sportanlagenreinigung, Verkehrsmittelreinigung, Restaurantreinigung,
+          Reinigung von Vereinsräumlichkeiten, Eventreinigung</b
+        >
+        und <b>Messereinigung.</b>
+      </p>
+    </div>
+  </div>
+</template>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -3,16 +3,19 @@ import Header from "../components/Header.vue";
 import Hero from "../components/Hero.vue";
 import TopThree from "../components/TopThree.vue";
 import Information from "../components/Information.vue";
+import Leistungen from "../components/Leistungen.vue";
 </script>
 
 <template>
   <div>
-    <Header />
+    <Header class="mb-12" />
     <Hero />
 
     <div class="my-12 grid gap-10 bg-[#E5F5F7] px-5 py-16 md:px-20 lg:px-24">
       <TopThree />
       <Information />
     </div>
+
+    <Leistungen class="mb-12 px-5" />
   </div>
 </template>
