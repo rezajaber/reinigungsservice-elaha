4c6c06752d3ca22ea7a8a7bd6f4e934635d5d7da
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-23 04:00:15 +0200

    Change Testimonials Component

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,5 +1,109 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div
+    class="mx-auto mt-6 w-full max-w-[1080px] rounded-lg bg-white px-8 pb-8 pt-4 shadow-lg dark:bg-gray-800"
+  >
+    <div class="-mt-16 flex justify-center md:justify-end">
+      <img
+        class="h-20 w-20 rounded-full border-2 border-secondary object-cover dark:border-blue-400"
+        alt="Testimonial avatar"
+        src="../assets/img/logo.png"
+      />
+    </div>
+
+    <div class="mt-4 grid gap-3 md:place-items-start">
+      <!-- STAR RATING -->
+      <div class="grid gap-1 text-start text-sm font-light">
+        <p class="text-center md:text-start">Top Service für Sie</p>
+        <div class="flex items-center justify-center">
+          <svg
+            class="me-1 h-4 w-4 text-[#F0AD4E]"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path
+              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+            />
+          </svg>
+          <svg
+            class="me-1 h-4 w-4 text-[#F0AD4E]"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path
+              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+            />
+          </svg>
+          <svg
+            class="me-1 h-4 w-4 text-[#F0AD4E]"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path
+              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+            />
+          </svg>
+          <svg
+            class="me-1 h-4 w-4 text-[#F0AD4E]"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path
+              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+            />
+          </svg>
+          <svg
+            class="me-1 h-4 w-4 text-gray-400 dark:text-gray-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path
+              d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
+            />
+          </svg>
+          <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+            4.95
+          </p>
+          <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+            out of
+          </p>
+          <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+            5
+          </p>
+        </div>
+      </div>
+
+      <!-- TEXT -->
+      <h1
+        class="break-words text-center text-[26px] font-bold leading-7 tracking-wide text-primary md:text-start"
+      >
+        Willkommen bei S&N Gebäudereinigung Hamburg
+      </h1>
+
+      <p class="text-center font-light leading-7 tracking-wide md:text-start">
+        Unsere N&T-Reinungsfirma ist Ihre vertrauenswürdige Adresse für
+        herausragende Dienstleistungen in der Reinigungsbranche. Unser
+        engagiertes Team von Experten setzt auf Professionalität, Präzision und
+        Nachhaltigkeit. Mit unserer langjährigen Erfahrung und unserem strengen
+        Fokus auf Qualität bieten wir maßgeschneiderte Reinigungslösungen für
+        eine Vielzahl von Gebäudetypen an. Von Büros über Gewerbeimmobilien bis
+        hin zu industriellen Anlagen – wir garantieren nicht nur makellose
+        Sauberkeit, sondern auch den Schutz der Umwelt durch den Einsatz
+        umweltfreundlicher Methoden. Unser Ziel ist es, durch unseren
+        erstklassigen Service und unsere innovativen Ansätze eine Atmosphäre zu
+        schaffen, in der Sie sich wohl und Ihre Immobilien geschützt fühlen.
+      </p>
+    </div>
+  </div>
 </template>
