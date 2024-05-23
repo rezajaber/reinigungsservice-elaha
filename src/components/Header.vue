516332ecccfbceacaff237a7d4d6b85874c9cdc0
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-23 03:59:57 +0200

    Change Header Component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Menu, Mail, Phone } from "lucide-vue-next";
+import { Menu, Mail, Phone, PhoneCall } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,21 +9,34 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const scrollTo = (id: string, behavior: ScrollBehavior = "auto") => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior });
+  }
+};
 </script>
 
 <template>
   <div class="shadow-md">
     <div
-      class="mx-auto grid h-20 max-w-[1080px] px-4 transition-all duration-300 ease-in-out lg:h-28 lg:px-28 xl:px-0"
+      class="mx-auto grid h-20 max-w-[1080px] px-4 transition-all duration-300 ease-in-out lg:h-24 lg:px-28 xl:px-0"
     >
       <div class="flex items-center justify-between">
-        <img src="../assets/img/logo.svg" class="w-48 lg:w-72" alt="Logo" />
+        <img src="../assets/img/logo.png" class="w-10 lg:w-14" alt="Logo" />
 
         <!-- MOBILE MENU -->
         <div class="flex lg:hidden">
           <Dialog>
             <DialogTrigger as-child>
-              <Button variant="ghost"><Menu /></Button>
+              <Menu class="cursor-pointer" />
             </DialogTrigger>
 
             <DialogContent class="max-w-xs rounded-lg md:max-w-md">
@@ -35,22 +48,48 @@ import {
               </DialogHeader>
 
               <div class="grid gap-2.5">
-                <Button type="button" variant="secondary">
+                <Button type="button">
                   <Mail class="mr-2 w-5" /> reinigungsservice@gmail.com
                 </Button>
-                <Button type="button" variant="secondary">
+                <Button type="button">
                   <Phone class="mr-2 w-5" /> 01523-5884265
                 </Button>
-                <Button class="bg-green-700">WhatsApp</Button>
+                <Button>WhatsApp</Button>
               </div>
             </DialogContent>
           </Dialog>
         </div>
 
         <!-- LAPTOP HEADER -->
-        <div class="hidden items-center gap-6 lg:flex">
-          <span class="text-md font-semibold">info@sn-hamburg.de</span>
-          <span class="text-md font-semibold">0151-283402930</span>
+        <div class="hidden items-center gap-2 lg:flex">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  @click="scrollTo('contact-us', 'smooth')"
+                  class="text-md font-semibold"
+                  ><Mail
+                /></Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>reinigungsservice@gmail.com</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  @click="scrollTo('contact-us', 'smooth')"
+                  class="text-md font-semibold"
+                  ><PhoneCall
+                /></Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>01523-5884265</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
           <Button class="px-8">WhatsApp</Button>
         </div>
       </div>
