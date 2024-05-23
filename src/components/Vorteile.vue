6eba28f97d2fd29e9e2b245c3877c53354d97e30
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-23 04:00:31 +0200

    Change Vorteile Component

--- src/components/Vorteile.vue
+++ src/components/Vorteile.vue
@@ -1,32 +1,55 @@
 <script setup lang="ts">
-import { HeartHandshake } from "lucide-vue-next";
+import { SearchCheck, ClipboardList, HeartHandshake } from "lucide-vue-next";
+
 import Button from "./ui/button/Button.vue";
+
+const vorteile = [
+  {
+    id: 1,
+    icon: "SearchCheck",
+    title: "Unsere Präzision",
+    description:
+      "Unsere Arbeit zeichnet sich durch höchste Präzision aus, um sicherzustellen, dass jede Aufgabe mit äußerster Genauigkeit und Sorgfalt ausgeführt wird.",
+  },
+  {
+    id: 2,
+    icon: "ClipboardList",
+    title: "Unsere Flexibilität",
+    description:
+      "Dank unserer Vielfalt an Services und unserer Flexibilität können wir uns leicht an die spezifischen Anforderungen unserer Kunden anpassen und maßgeschneiderte Lösungen liefern.",
+  },
+  {
+    id: 3,
+    icon: "HeartHandshake",
+    title: "Unser Kundenservice",
+    description:
+      "Wir legen großen Wert auf exzellenten Kundenservice, um sicherzustellen, dass unsere Kunden jederzeit zufrieden sind. Unser engagiertes Team steht Ihnen zur Seite, um Ihre Anliegen zu hören und Ihnen zu helfen.",
+  },
+];
+
+const icons = [SearchCheck, ClipboardList, HeartHandshake];
 </script>
 
 <template>
   <div>
     <div class="mx-auto max-w-[1080px]">
-      <h1
-        class="mb-10 break-words text-center text-[26px] font-bold leading-7 tracking-wide text-primary"
-      >
-        Ihre Vorteile bei S&N Gebäuderinigung auf einen Blick
-      </h1>
-
-      <div class="mb-5 grid gap-6 lg:grid-cols-3">
+      <div class="grid gap-6 lg:grid-cols-3">
         <div
-          v-for="i in 3"
-          :key="i"
-          class="grid place-items-center rounded-lg border-2 border-primary px-5 py-7 md:px-7 md:py-10"
+          v-for="(vorteil, index) in vorteile"
+          :key="vorteil.id"
+          class="flex flex-col items-center rounded-lg bg-white px-5 py-7 shadow-lg md:px-7 md:py-10"
         >
-          <HeartHandshake class="mb-6 scale-150 fill-white stroke-primary" />
+          <component
+            :is="icons[index]"
+            class="mb-4 scale-150 fill-white stroke-primary"
+          />
           <h3
             class="mb-2 text-center text-xl font-semibold leading-7 tracking-wide"
           >
-            Eigenes Reinigungsequipment
+            {{ vorteil.title }}
           </h3>
           <p class="text-center font-light leading-7 tracking-wide">
-            Wir setzen auf unsere eigenen, hochwertigen Reinigungsprodukte und
-            -geräte für erstklassige Sauberkeit.
+            {{ vorteil.description }}
           </p>
         </div>
       </div>
